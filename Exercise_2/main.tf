195cdbd65efeee68e7b8112c5956e4637faf9449
LongCa <thanhlong1543@gmail.com>
2023-02-28 12:29:51 +0700

    add log policy

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -21,6 +21,31 @@ resource "aws_iam_role" "iam_for_lambda" {
   })
 }
 
+resource "aws_iam_policy" "lambda_iam_policy" {
+  name        = "lambda_iam_policy"
+  path        = "/"
+  description = "AWS IAM Policy for aws lambda"
+  policy      = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "arn:aws:logs:*:*:*",
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_iam_policy.arn
+}
+
 
 data "archive_file" "zip_python_code" {
   type        = "zip"
@@ -34,6 +59,7 @@ resource "aws_lambda_function" "lambda_by_terraform" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "greet_lambda.lambda_handler"
   runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   environment {
     variables = {
       greeting = "environment greeting"
