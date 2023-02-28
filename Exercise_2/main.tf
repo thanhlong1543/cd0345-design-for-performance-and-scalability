7b526a94ec9ddb17dba2bbdc9cf07916bc416da5
LongCa <thanhlong1543@gmail.com>
2023-02-28 12:08:44 +0700

    update

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
   region     = var.aws_region
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_function_role"
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -21,30 +21,6 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_policy" "lambda_iam_policy" {
-  name        = "lambda_iam_policy"
-  path        = "/"
-  description = "AWS IAM Policy for aws lambda"
-  policy      = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource" : "arn:aws:logs:*:*:*",
-        "Effect" : "Allow"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_iam_policy.arn
-}
 
 data "archive_file" "zip_python_code" {
   type        = "zip"
@@ -55,10 +31,9 @@ data "archive_file" "zip_python_code" {
 resource "aws_lambda_function" "lambda_by_terraform" {
   filename      = "${path.module}/greeting-python.zip"
   function_name = "lambda_by_terraform"
-  role          = aws_iam_role.lambda_role.arn
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "greet_lambda.lambda_handler"
   runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   environment {
     variables = {
       greeting = "environment greeting"
