42b8dc2ddd493dc3a704fc3e0887a240bbee644a
LongCa <thanhlong1543@gmail.com>
2023-02-28 12:21:20 +0700

    no message

--- Exercise_2/main.tf
+++ Exercise_2/main.tf
@@ -24,12 +24,12 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "zip_python_code" {
   type        = "zip"
-  source_dir  = "${path.module}/"
-  output_path = "${path.module}/greeting-python.zip"
+  source_dir  = "${path.module}/python"
+  output_path = "${path.module}/python/greeting-python.zip"
 }
 
 resource "aws_lambda_function" "lambda_by_terraform" {
-  filename      = "${path.module}/greeting-python.zip"
+  filename      = "${path.module}/python/greeting-python.zip"
   function_name = "lambda_by_terraform"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "greet_lambda.lambda_handler"
